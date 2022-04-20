7d1b04795a36ea1fa07c5c2d1932c13298bd1b6f
Rudolph1542 <rodolf-12@hotmail.com>
2022-04-20 12:58:48 -0500

    commit1

--- server.py
+++ server.py
@@ -9,13 +9,23 @@ def aleatorio():
         x+="%02x " % (a[i])
     return x
 
+CHUNK_SIZE = 4096
+send=0
 soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 soc.bind(('127.0.0.1',65432))
-soc.listen()
+soc.listen(1)
 x,ad=soc.accept()
-print(f'{ad} conectado')
-aleatorioo=aleatorio().encode()
-print(f'se envia aleatorio: {aleatorioo}')
-x.sendall(aleatorioo)
-x.close()
-soc.close()
+print(f'{ad} Conectado')
+if(send==1):
+    aleatorioo=aleatorio().encode()
+    print(f'se envia aleatorio: {aleatorioo}')
+    x.sendall(aleatorioo)
+    x.close()
+    soc.close()
+else:
+    with open('recived.txt', "wb") as filee:
+        chunk = x.recv(CHUNK_SIZE)
+        while chunk:
+            filee.write(chunk)
+            chunk = x.recv(CHUNK_SIZE)
+        soc.close()
